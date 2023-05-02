051a4589e7b675dd766ecd79fc0c4272b00eb1b3
alerrandro borges <alerrandrokaton@gmail.com>
2023-05-02 18:02:01 -0300

    feat: regex global

--- src/RegexBr/Regex.php
+++ src/RegexBr/Regex.php
@@ -6,8 +6,9 @@ use Alerrandro\Regex\interface\iRegex as InterfaceIRegex;
 
 class Regex implements InterfaceIRegex
 {
-    public static function telefone(string $telefone): string
+    public static function regex(string $string): string
     {
-        return preg_replace("/([^\p{L}\p{N}]|\s+)/u", '', $telefone);
+        return preg_replace("/([^\p{L}\p{N}]|\s+)/u", '', $string);
     }
+
 }
